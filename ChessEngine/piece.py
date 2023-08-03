bdbe36bd0ceddbf4d1873ac94a822ce5c3501fa7
Krystian Jachna <krysti4nape@gmail.com>
2023-08-03 03:47:38 +0200

    piece class preview

--- ChessEngine/piece.py
+++ ChessEngine/piece.py
@@ -1,8 +1,65 @@
 # Piece service
 
-class Piece():
-    def __init__(self) -> None:
+from abc import ABC, abstractclassmethod
+from ChessEngine.images_loader import ImagesLoader
+
+class Piece(ABC):
+    def __init__(self, x, y, surface) -> None:
+        self.x, self.y = x, y
+        self.surface = surface
+
+    def draw(self, screen):
+        self.screen.blit(self.surface, (self.x, self.y))
+
+    def get_possible_moves():
         pass
+
+
+class King(Piece):
+    def __init__(self, x, y, surface) -> None:
+        super().__init__(x, y, surface)
+
+    def get_possible_moves():
+        pass
+        
+class Queen(Piece):
+    def __init__(self, x, y, surface) -> None:
+        super().__init__(x, y, surface)
+
+    def get_possible_moves():
+        pass
+
+class Roock(Piece):
+    def __init__(self, x, y, surface) -> None:
+        super().__init__(x, y, surface)
+
+    def get_possible_moves():
+        pass
+
+class Bishop(Piece):
+    def __init__(self, x, y, surface) -> None:
+        super().__init__(x, y, surface)
+
+    def get_possible_moves():
+        pass
+
+class Knight(Piece):
+    def __init__(self, x, y, surface) -> None:
+        super().__init__(x, y, surface)
+
+    def get_possible_moves():
+        pass
+
+class BlackPawn(Piece):
+    def __init__(self, x, y, surface) -> None:
+        super().__init__(x, y, surface)
+
+    def get_possible_moves():
+        pass
+
+class WhitePawn(Piece):
+    def __init__(self, x, y, surface) -> None:
+        super().__init__(x, y, surface)
 
     def get_possible_moves():
         pass
